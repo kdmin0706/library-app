8f2705973af28199fec60f40a6a1699f8db694bf
kdmin0706 <gxxk76@gmail.com>
2024-04-29 10:07:17 +0900

    [feat]: User Info 생성   - UserController Layer 생성   - User Dto 생성 (request, response)   - User Domain 생성

---
+++ src/main/java/com/group/libraryapp/dto/user/request/UserCreateRequest.java
@@ -0,0 +1,24 @@
+package com.group.libraryapp.dto.user.request;
+
+public class UserCreateRequest {
+
+  private String name;
+  private Integer age;
+
+  public UserCreateRequest() {
+  }
+
+  public UserCreateRequest(String name, Integer age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+}
