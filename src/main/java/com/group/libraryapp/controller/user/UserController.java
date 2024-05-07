4f66ef555afe979b55f50b8cee55ddfe38ab7ba9
kdmin0706 <gxxk76@gmail.com>
2024-05-07 10:58:57 +0900

    [refactor]: ServiceV1 -> ServiceV2 변경

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -3,7 +3,7 @@ package com.group.libraryapp.controller.user;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
-import com.group.libraryapp.service.user.UserServiceV1;
+import com.group.libraryapp.service.user.UserServiceV2;
 import java.util.List;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-  private final UserServiceV1 userService;
+  private final UserServiceV2 userService;
 
-  public UserController(UserServiceV1 userService) {
+  public UserController(UserServiceV2 userService) {
     this.userService = userService;
   }
 
